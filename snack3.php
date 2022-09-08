d4c5faf5f7e052a2f3f2a79664dd5719e86b209b
mauro-angel99 <angelottimauro@gmail.com>
2022-09-08 17:21:12 +0200

    snack 3

---
+++ snack3.php
@@ -0,0 +1,77 @@
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+$dates = array_keys($posts);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <?php for ($i = 0; $i < count($dates); $i++) {
+            $date = $dates[$i];
+            $post = $posts[$date]
+         ?>
+        <section>
+            <h2><?= $date ?></h2>
+            <?php for ($j = 0; $j < count($post); $j++) { ?>
+            <article>
+                <h3><?= $post[$j]['title'] ?></h3>
+                <address><?= $post[$j]['author'] ?></address>
+                <p><?= $post[$j]['text'] ?></p>
+            </article>
+            <?php } ?>
+        </section>
+        <?php } ?>
+    </div>
+</body>
+
+</html>
